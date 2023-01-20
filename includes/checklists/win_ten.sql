df5450207abc77ed968f1c91d78a6208aa82b86a
Spencer McConnell <spencer.mcconnell2003@gmail.com>
2023-01-20 10:19:40 -0700

    Created new offical running checklist for ubu, server

--- includes/checklists/win_ten.sql
+++ includes/checklists/win_ten.sql
@@ -1,78 +1,37 @@
--- phpMyAdmin SQL Dump
--- version 4.9.10
--- https://www.phpmyadmin.net/
---
--- Host: db5010816561.hosting-data.io
--- Generation Time: Jan 20, 2023 at 07:38 AM
--- Server version: 8.0.26
--- PHP Version: 7.0.33-0+deb9u12
-
---SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
---SET AUTOCOMMIT = 0;
---START TRANSACTION;
---SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `dbs9150826`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `win_ten`
---
-
---CREATE TABLE `win_ten` (
---  `id` int NOT NULL,
---  `item` varchar(10000) COLLATE utf8mb4_general_ci NOT NULL
---) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `win_ten`
---
-
 INSERT INTO `win_ten` (`item`) VALUES
 ('Enable hidden files and file extensions in file system view options <a href=\"#filesystem\">File System</a>'),
 ('Enable the firewall <a href=\"#network\">Network Security</a>'),
 ('Enable/verify antivirus is running <a href=\"#gpo\">Global Domain Policy</a>'),
 ('Enable/verify that Windows Event Log service is running (services.msc) <a href=\"#services\">Services & Processes</a>'),
 ('Enable/verify Windows Update service/Software Updates are running.  (START SYSTEM UPDATES) <a href=\"#software\">System Updates &amp; Software</a>'),
-('EVALUATE THE FORENSIC QUESTIONS\r\n        Forensic question generally asks for a directory where an unauthorized file or user was found. Check FORENSICS before removing anything!\r\n        DON\'T get bogged down in the forensics, but be careful not to delete evidence!'),
+('EVALUATE THE FORENSIC QUESTIONS\r\n        
+	Forensic question generally asks for a directory where an unauthorized file or user was found. 
+	Check FORENSICS before removing anything!\r\n        
+	DON\'T get bogged down in the forensics, but be careful not to delete evidence!'),
 ('SAVE EVIDENCE. If a file, immediately make a copy of the forensic file and place on the host system so we don\'t forget'),
 ('create a text file containing authorized users from the README'),
 ('Remove unauthorized users <a href=\"#lusrmgr\">Local User Management</a>'),
 ('Add any missing authorized users <a href=\"#lusrmgr\">Local User Management</a>'),
-('Remove non-admins from admin group/set them to standard user <a href=\"#lusrmgr\">Local User Management</a>');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `win_ten`
---
-
---ALTER TABLE `win_ten`
---  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `win_ten`
---
-
---ALTER TABLE `win_ten`
---  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
---COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+('Remove non-admins from admin group/set them to standard user <a href=\"#lusrmgr\">Local User Management</a>'),
+('Reconcile authorized users (lusrmgr.msc)'),
+('Remove unauthorized user directories c:\users or /home (BUT do your forensics first!) <a href="#filesystem">File System</a>'),
+('Look for unauthorized file shares (check the README and Forensics first) <a href="#filesystem">File System</a>'),
+('Check file permissions (read your readme file!!!!) <a href="#filesystem">File System</a>'),
+('Run CP_winscript.ps1 on Windows (PowerShell run as administrator)
+	<a href="CP_winscript.ps1" download="CP_winscript.ps1">Download CP_winscript.ps1</a> <strong>RIGHT CLICK ON LINK AND PRESS SAVE LINK AS(IF NO DOWNLOAD</strong> 
+	<a href="LGPO.exe" download="LGPO.exe">Download LGPO.exe</a> <strong> RIGHT CLICK ON LINK AND PRESS SAVE LINK AS (IF NO DOWNLOAD)</strong>
+	<a href="GptTmpl.inf" download="GptTmpl.inf">Download GptTmpl.inf</a> <strong>RIGHT CLICK ON LINK AND PRESS SAVE LINK AS(IF NO DOWNLOAD</strong>'),
+('Run the <strong>"lgpo.exe /s GptTmpl.inf"</strong> import '),
+('Group Policy <a href="#gpo">Global Domain Policy</a>'),
+('Update the default browser as indicated in the README file (e.g. Firefox) <a href="#browser">Browser Settings</a>'),
+('      Firefox pop-up blocker enabled'),
+('		Firefox automatically installs updates'),
+('Update all required, valid software to latest version as defined in README'),
+('Remove unsanctioned and unwanted programs. <a href="#software">System Updates &amp; Software</a>'),
+('Stop and disable unauthorized services
+    o run netstat -ant and check for listening services.  
+	  Check your services file to see what ports map to what services
+           Win: C:\Windows\System32\drivers\etc\services'),
+('Remove unauthorized files (mp3, m4b, .aa, .mkv, .m4r) (did you do forensics first?!)'),
+('Remove hacking tools (nmap, rainbowcrack, ophcrack,*crack*, xhydra, wireshark, openvpn, betternetVPN, *sploit*) (did you do forensics first?!)'),
+('Set remote access related settings (RDP, openSSH) defined in README');
